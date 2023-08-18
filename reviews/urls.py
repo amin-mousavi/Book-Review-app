ab0f846bc4f13ad4a9d658a9724146160499b05a
Amin Mousavi <seyedamin963@gmail.com>
2023-08-18 14:53:29 +0330

    Add static files to our project e.g logos and css file.

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('', views.index),
     path('books/', views.book_list, name='book_list'),
     path('books/<int:pk>/', views.book_detail, name='book_detail'),
 ]
